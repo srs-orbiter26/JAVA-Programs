cc594d4373bb91f7e6d63f5bdee6f798b32415e9
SOUMYA RANJAN SAHOO <105946301+srs-orbiter26@users.noreply.github.com>
2023-06-09 10:46:26 +0530

    Add files via upload

---
+++ array_1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class array_1{
+	public static void main(String args[]){
+		int []arr=new int[3];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		
+		for(int i=0;i<arr.length;i++){
+			System.out.println(arr[i]);
+		}
+	}
+}
